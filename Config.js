40b2cf537b7f0fdf131ba829cb3f4e803b06b338
Mark256-maker <wmax78347@gmail.com>
2025-03-13 00:23:38 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,12 +2,12 @@
 
 global.settings = {
   OWNER_NUMBERS: "256754310866", // your number with country code
-  OWNER_NAME: 'Mark Bot ', // your owner name 
+  OWNER_NAME: 'Marksoft Technology 🇺🇬', // your owner name 
   BOT_NAME: 'MarksoftBot', // your preferred bot name
   SESSION_ID: 'kord_ai-46J7YhEHeNAi', // replace your_sessionid with yours and make sure its inside the 
   INVINCIBLE_MODE: true, // toogle invincible mode 
   WORK_MODE: 'private',
-  TIME_ZONE: "Africa/Lagos",
+  TIME_ZONE: "Africa/Kampala",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
   AUTO_READ_STATUS: true,
@@ -18,12 +18,12 @@ global.settings = {
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
   MAX_COMMANDS_PER_MINUTE: 100,
   MAX_DOWNLOAD_SIZE: 100,
-  AUTO_LIKE_STATUS: true,
+  AUTO_LIKE_STATUS: false,
   AUTO_LIKE_EMOJI: '✨',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "MTN Mobile money🇺🇬", // the name of your preferred bank
+    acc_number: "+256780349774", // account number for your preferred bank
+    acc_name: "Mariam Kia, deposit 10ugx to get what's app bot plus panel" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
@@ -37,11 +37,11 @@ global.settings = {
   DELETE_WA_LINKS: false,
   ANTI_DELETE_ENABLED: true,
   ANTI_EDIT_ENABLED: true,
-  ANTI_EDIT_IN_CHAT: false,
+  ANTI_EDIT_IN_CHAT: true,
   ANTI_PM_SPAM: false, //pm permit 
   ANTI_PM_SPAM_COUNT: 6,
-  AUTO_TYPING: false, // if you wnat the bot to be typing always 
+  AUTO_TYPING: true, // if you wnat the bot to be typing always 
   AUTO_RECORDING: false, //if you want the bot to be recording always
-  ALWAYS_ONLINE: false,
+  ALWAYS_ONLINE: true,
 
 };
